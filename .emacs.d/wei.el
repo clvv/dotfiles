8ab9f738fe7df135731e98d0e66141997bc35899
Wei Dai <x@wei23.net>
2010-10-24 22:33:36 -0700

    Remove emacs files from nox branch

--- .emacs.d/wei.el
+++
@@ -1,8 +0,0 @@
-(require 'slime)
-(add-to-list 'slime-lisp-implementations '(sbcl ("/usr/bin/lisp")))
-(require 'smart-tab)
-(global-smart-tab-mode 1)
-(require 'vimpulse)
-(require 'vimpulse-surround)
-
-
