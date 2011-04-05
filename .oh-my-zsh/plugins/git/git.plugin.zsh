be344d7bd137794a6038e4c736e5e11ad44a0180
Wei Dai <x@wei23.net>
2011-04-05 03:42:50 -0700

    Update oh-my-zsh

--- .oh-my-zsh/plugins/git/git.plugin.zsh
+++ .oh-my-zsh/plugins/git/git.plugin.zsh
@@ -1,22 +1,39 @@
 # Aliases
 alias g='git'
+compdef g=git
 alias gst='git status'
+compdef _git gst=git-status
 alias gl='git pull'
+compdef _git gl=git-pull
 alias gup='git fetch && git rebase'
+compdef gup=git
 alias gp='git push'
+compdef _git gp=git-push
 alias gd='git diff | mate'
+# WTF is mate??
+compdef _git gd=git-diff
 alias gdv='git diff -w "$@" | vim -R -'
+compdef gdv=git
 alias gc='git commit -v'
+compdef gc=git
 alias gca='git commit -v -a'
+compdef gca=git
 alias gco='git checkout'
+compdef _git gco=git-checkout
 alias gb='git branch'
+compdef _git gb=git-branch
 alias gba='git branch -a'
+compdef gba=git
 alias gcount='git shortlog -sn'
+compdef gcount=git
 alias gcp='git cherry-pick'
+compdef _git gcp=git-cherry-pick
 alias glg='git log --stat --max-count=5'
+compdef _git glg=git-log
 
 # Git and svn mix
 alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
+compdef git-svn-dcommit-push=git
 
 #
 # Will return the current branch name
@@ -29,5 +46,8 @@ function current_branch() {
 
 # these aliases take advantage of the previous function
 alias ggpull='git pull origin $(current_branch)'
+compdef ggpull=git
 alias ggpush='git push origin $(current_branch)'
+compdef ggpush=git
 alias ggpnp='git pull origin $(current_branch) && git push origin $(current_branch)'
+compdef ggpnp=git
