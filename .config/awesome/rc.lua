bfe90f58679c9ab7ffcf01d54a087a464d9c8e07
Wei Dai <x@wei23.net>
2011-11-24 13:39:45 -0800

    Fixes for urxvt

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -41,7 +41,7 @@ end
 beautiful.init("/usr/share/awesome/themes/zenburn/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "urxvtc"
+terminal = os.getenv('HOME') .. "/bin/" .. "urxvtcd"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
