93a9b402eedd6b6da38499aad3c6d2ea56b0d962
Wei Dai <x@wei23.net>
2011-11-24 18:27:09 -0800

    Tweak zsh key bindings

--- .zsh/bindkey.zsh
+++ .zsh/bindkey.zsh
@@ -1,30 +1,30 @@
 bindkey -e
-bindkey '\ew' kill-region
-bindkey -s '\el' "ls"
-bindkey -s '\e.' ".."
+bindkey -s '^[l' 'ls'
+bindkey -s '^[>' '..'
 bindkey '^r' history-incremental-search-backward
-bindkey "^[[5~" up-line-or-history
-bindkey "^[[6~" down-line-or-history
+bindkey '^[[5~' up-line-or-history
+bindkey '^[[6~' down-line-or-history
 
 bindkey '^[[A' up-line-or-search
 bindkey '^[[B' down-line-or-search
 
-bindkey "^[[H" beginning-of-line
-bindkey "^[[1~" beginning-of-line
-bindkey "^[[F"  end-of-line
-bindkey "^[[4~" end-of-line
+bindkey '^[[H' beginning-of-line
+bindkey '^[[1~' beginning-of-line
+bindkey '^[[F'  end-of-line
+bindkey '^[[4~' end-of-line
 bindkey ' ' magic-space
 
 bindkey '^[[Z' reverse-menu-complete
 
-bindkey "^[[3~" delete-char
-bindkey "^[3;5~" delete-char
-bindkey "\e[3~" delete-char
+bindkey '^[[3~' delete-char
+bindkey '^[3;5~' delete-char
+bindkey '\e[3~' delete-char
 
-bindkey "^[m" copy-prev-shell-word
+bindkey '^[m' copy-prev-shell-word
+bindkey '^[.' insert-last-word
 
-bindkey "^K" history-beginning-search-backward
-bindkey "^J" history-beginning-search-forward
-bindkey "^[^I" _history-complete-older
-bindkey "^[^[^I" _history-complete-newer
+bindkey '^K' history-beginning-search-backward
+bindkey '^J' history-beginning-search-forward
+bindkey '^[^I' _history-complete-older
+bindkey '^[^[^I' _history-complete-newer
 
