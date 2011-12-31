130a4a03e6e258e7ee58676fa56cd17583c969b9
Wei Dai <x@wei23.net>
2011-12-31 00:52:37 -0800

    Stop using git prompt for performance issues

--- .zsh/theme.zsh
+++ .zsh/theme.zsh
@@ -2,10 +2,5 @@ local return_code="%(?..%{$fg[red]%}%?%{$reset_color%})"
 
 PROMPT='%{$fg[green]%}[%n %m] %{$fg[yellow]%}%c %{$fg[red]%}%#%{$reset_color%} '
 PROMPT2='%{$fg[red]%}\ %{$reset_color%}'
-RPS1='%{$fg[blue]%}%~ $(git_prompt_info)%{$reset_color%} ${return_code} '
-
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$reset_color%}%{$fg[blue]%}["
-ZSH_THEME_GIT_PROMPT_SUFFIX="]%{$reset_color%} "
-ZSH_THEME_GIT_PROMPT_CLEAN=""
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[green]%}*%{$fg[blue]%}"
+RPS1='%{$fg[blue]%}%~ %{$reset_color%}${return_code}'
 
