dd467a45c4f4e7e0e72ed8e10a97976875648ddb
Wei Dai <x@wei23.net>
2012-01-26 01:06:43 -0800

    Tweak git prompt for non git use

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,4 +1,4 @@
-PROMPT='%{$fg[green]%}[%n %m] %{$fg[yellow]%}[%c $(_gb)] %{$fg[red]%}%#%{$reset_color%} '
+PROMPT='%{$fg[green]%}[%n %m] %{$fg[yellow]%}[%c$(_gb)] %{$fg[red]%}%#%{$reset_color%} '
 PROMPT2='%{$fg[red]%}\ %{$reset_color%}'
 RPS1='%{$fg[blue]%}%~ %{$reset_color%}%(?..%{$fg[red]%}%?%{$reset_color%})'
 
